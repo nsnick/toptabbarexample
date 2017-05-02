9b902f30378c4e41346a21858cd14ec17ca093c9
Nick Wilkerson <nickwilkerson@gmail.com>
2017-05-01 19:55:37 -0700

    Initial Commit

---
+++ TopTabBarExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TopTabBarExample
+//
+//  Created by Nick Wilkerson on 5/1/17.
+//  Copyright © 2017 Nick Wilkerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
